787d1cfe9ee42614fac5be60a90365b6e773f549
DCluseau <daphne.cluseau@fms-ea.com>
2026-01-30 15:14:26 +0100

    Created customer component and did html part but problem with displaying page

---
+++ src/app/components/customer/customer-component/customer-component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-customer-component',
+  imports: [FormsModule],
+  templateUrl: './customer-component.html',
+  styleUrl: './customer-component.css',
+})
+export class CustomerComponent {
+
+  customer(){
+
+  }
+
+}
